e94e78cbf7884509e8702390a9c69da00465a81d
Christian Huck <chris@huck-it.de>
2018-06-05 23:32:38 +0200

    added access to notification handler

--- Example/RxRealm_Tests/RxRealmObjectTests.swift
+++ Example/RxRealm_Tests/RxRealmObjectTests.swift
@@ -14,6 +14,78 @@ import RxRealm
 
 class RxRealmObjectTests: XCTestCase {
 
+    
+    func testToken() {
+        let realm = realmInMemory(#function)
+        
+        //create object
+        let idValue = 1024
+        let obj = UniqueObject(idValue)
+        try! realm.write {
+            realm.add(obj)
+        }
+
+        
+        // Create an expectation for a background download task.
+        let expectation = XCTestExpectation(description: "Download apple.com home page")
+
+        let objectNotifications = Observable.from(object: obj, tokenReceiver: { token in
+                expectation.fulfill()
+        }).map{ $0.id }
+        
+        
+        XCTAssertEqual(try! objectNotifications.toBlocking().first()!, 1024)
+        
+         wait(for: [expectation], timeout: 10.0)
+        
+        
+    }
+    
+    func testIgnoreToken() {
+        let realm = realmInMemory(#function)
+        
+        var notificationToken: NotificationToken? = nil
+        
+        //create object
+        let idValue = 1024
+        let obj = UniqueObject(idValue)
+        try! realm.write {
+            realm.add(obj)
+        }
+        
+        let disposeBag = DisposeBag()
+        
+        let objectNotifications = Observable.from(object: obj, tokenReceiver: { token in
+            notificationToken = token
+        }).map{ $0.name }
+        
+        var finalNameValue = ""
+        
+        
+        objectNotifications.subscribe(onNext: { (snapShotID) in
+                finalNameValue = snapShotID
+        }).disposed(by: disposeBag)
+        
+        
+        let expectation = XCTestExpectation(description: "wait for id")
+        
+        Observable<Int>.interval(3, scheduler: MainScheduler.asyncInstance).take(1).map{_ in return ()}.subscribe(onNext: { _ in
+            print("value  is now \(finalNameValue)")
+            if finalNameValue != "ignored" {
+                expectation.fulfill()
+            }
+            else{
+                XCTFail()
+            }
+        }).disposed(by: disposeBag)
+        try! realm.beginWrite()
+            obj.name = "ignored"
+        try! realm.commitWrite(withoutNotifying: [notificationToken!])
+        
+         wait(for: [expectation], timeout: 4)
+    }
+        
+        
     func testObjectChangeNotifications() {
         let realm = realmInMemory(#function)
 
